1db29765b1692699bb3ca8240f6db20c4fe57cb6
Patrick Vinton <patrickvinton@hotmail.com>
2023-12-14 12:34:47 -0600

    AAD integration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -11,6 +11,13 @@ Amplify.configure({
     userPoolId: import.meta.env.VITE_USER_POOL_ID,
     userPoolWebClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
     region: import.meta.env.VITE_API_REGION,
+    oauth: {
+      domain: "https://pvinton-sandbox.auth.us-east-1.amazoncognito.com",
+      scope: ["email", "openid", "aws.cognito.signin.user.admin", "profile"],
+      redirectSignIn: "https://main.d29s4hlgft4djr.amplifyapp.com/",
+      redirectSignOut: "https://main.d29s4hlgft4djr.amplifyapp.com/",
+      responseType: "code"
+    }
   },
   API: {
     endpoints: [
