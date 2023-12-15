b89d7a47be7df7059e166ac16f91bcd51d50de98
Patrick Vinton <patrickvinton@hotmail.com>
2023-12-15 11:54:23 -0600

    sso16

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,124 +1,66 @@
-import "./index.css";
-import { Amplify, Hub, Auth } from "aws-amplify";
-import { useEffect, useState } from "react";
-
-// import { withAuthenticator } from "@aws-amplify/ui-react";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Layout from "./routes/layout";
-// import Documents from "./routes/documents";
-// import Chat from "./routes/chat";
+import React, { useEffect, useState } from "react";
+import { Amplify } from "aws-amplify";
+import { Hub } from "aws-amplify/utils";
+import { signInWithRedirect, getCurrentUser } from "aws-amplify/auth";
+import { AuthUser } from "aws-amplify/auth";
 
 Amplify.configure({
   Auth: {
-    userPoolId: import.meta.env.VITE_USER_POOL_ID,
-    userPoolWebClientId: import.meta.env.VITE_USER_POOL_CLIENT_ID,
-    region: import.meta.env.VITE_API_REGION,
-    oauth: {
-      domain: "pvinton-sandbox.auth.us-east-1.amazoncognito.com",
-      scope: ["email", "openid", "aws.cognito.signin.user.admin", "profile"],
-      redirectSignIn: "https://main.d29s4hlgft4djr.amplifyapp.com/",
-      redirectSignOut: "https://main.d29s4hlgft4djr.amplifyapp.com/",
-      responseType: "code"
-    }
-  },
-  API: {
-    endpoints: [
-      {
-        name: "serverless-pdf-chat",
-        endpoint: import.meta.env.VITE_API_ENDPOINT,
-        region: import.meta.env.VITE_API_REGION,
-        custom_header: async () => {
-          return {
-            Authorization: `Bearer ${(await Auth.currentSession())
-              .getIdToken()
-              .getJwtToken()}`,
-          };
+    Cognito: {
+      loginWith: {
+        oauth: {
+          redirectSignIn: [
+            "https://main.d29s4hlgft4djr.amplifyapp.com/",
+          ],
+          ...oauthConfig
         },
       },
-    ],
+     ...userPoolConfig
+    },
   },
 });
 
-// let router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         index: true,
-//         Component: Documents,
-//       },
-//       {
-//         path: "/doc/:documentid/:conversationid",
-//         Component: Chat,
-//       },
-//     ],
-//   },
-// ]);
-
-function checkUser() {
-  Auth.currentAuthenticatedUser()
-    .then(user => console.log({ user }))
-    .catch(err => console.log(err))
-}
-
-const openSaml = () => {
-  window.location.assign(
-    'https://pvinton-sandbox.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=3gpscd72mrd07hjba3gqmeci26&redirect_uri=https://main.d29s4hlgft4djr.amplifyapp.com'
-  );
-};
-
-const signIn = async () => {
-  try {
-    console.log('before federatedSignin')
-    await Auth.federatedSignIn();
-    console.log('after federatedSignin')
-  } catch (error) {
-    console.log("error signing in", error);
-  }
-};
-
 function App() {
-  const [user, setUser] = useState(null);
-
-  Hub.listen('auth', (data) => {
-    console.log('got auth event')
-    const { payload } = data
-    console.log(payload)
-  })
+  const [user, setUser] = useState<AuthUser | null>(null);
+  const [error, setError] = useState<unknown>(null);
+  const [customState, setCustomState] = useState<string | null>(null);
 
   useEffect(() => {
-    console.log('in useEffect')
-    getUser().then((userData) => setUser(userData));
+    const unsubscribe = Hub.listen("auth", ({ payload }) => {
+      switch (payload.event) {
+        case "signInWithRedirect":
+          getUser();
+          break;
+        case "signInWithRedirect_failure":
+          setError("An error has ocurred during the Oauth flow.");
+          break;
+        case "customOAuthState":
+          setCustomState(payload.data);
+          break;
+      }
+    });
+
+    getUser();
+
+    return unsubscribe;
   }, []);
 
-  function getUser() {
-    return Auth.currentAuthenticatedUser()
-      .then((userData) => userData)
-      .catch(() => console.log("Not signed in"));
-  }
+  const getUser = async (): Promise<void> => {
+    try {
+      console.log('getting currentUser')
+      const currentUser = await getCurrentUser();
+      console.log(currentUser)
+      setUser(currentUser);
+    } catch (error) {
+      console.error(error);
+      console.log("Not signed in");
+    }
+  };
 
   return (
     <div className="App">
-      <h1>
-        Welcome{" "}
-        {user
-          ? JSON.stringify(user)
-          : "Oops! Not signed in"}
-      </h1>
-      <header className="App-header">
-        <button onClick={checkUser}>Check User</button>
-        <button onClick={() => Auth.federatedSignIn()}>Sign In 1</button>
-        <button onClick={openSaml}>Sign In 2</button>
-        <button onClick={signIn}>Sign In 3</button>
-      </header>
+      <button onClick={() => signInWithRedirect()}>Open Hosted UI</button>
+      <div>{user?.username}</div>
     </div>
-    // <RouterProvider router={router} />;
-  )
+  );
 }
-
-// export default withAuthenticator(App, { 
-//   hideSignUp: true,
-//   socialProviders: ['apple']
-// });
-export default App
