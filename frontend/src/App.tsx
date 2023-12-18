fc962785ea3519c7316ec4024c1fa7f09b911c2c
Patrick Vinton <patrickvinton@hotmail.com>
2023-12-18 12:23:29 -0600

    back to simple

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,12 @@
 import "./index.css";
-import { Amplify, Hub, Auth } from "aws-amplify";
-import { useEffect, useState } from "react";
+import { Amplify, Auth } from "aws-amplify";
+// import { useEffect, useState } from "react";
 
-// import { withAuthenticator } from "@aws-amplify/ui-react";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import Layout from "./routes/layout";
-// import Documents from "./routes/documents";
-// import Chat from "./routes/chat";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./routes/layout";
+import Documents from "./routes/documents";
+import Chat from "./routes/chat";
 
 Amplify.configure({
   Auth: {
@@ -39,87 +39,86 @@ Amplify.configure({
   },
 });
 
-// let router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Layout />,
-//     children: [
-//       {
-//         index: true,
-//         Component: Documents,
-//       },
-//       {
-//         path: "/doc/:documentid/:conversationid",
-//         Component: Chat,
-//       },
-//     ],
-//   },
-// ]);
+let router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        index: true,
+        Component: Documents,
+      },
+      {
+        path: "/doc/:documentid/:conversationid",
+        Component: Chat,
+      },
+    ],
+  },
+]);
 
-function checkUser() {
-  Auth.currentAuthenticatedUser()
-    .then(user => console.log({ user }))
-    .catch(err => console.log(err))
-}
+// function checkUser() {
+//   Auth.currentAuthenticatedUser()
+//     .then(user => console.log({ user }))
+//     .catch(err => console.log(err))
+// }
 
-const openSaml = () => {
-  window.location.assign(
-    'https://pvinton-sandbox.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=3gpscd72mrd07hjba3gqmeci26&redirect_uri=https://main.d29s4hlgft4djr.amplifyapp.com'
-  );
-};
+// const openSaml = () => {
+//   window.location.assign(
+//     'https://pvinton-sandbox.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=3gpscd72mrd07hjba3gqmeci26&redirect_uri=https://main.d29s4hlgft4djr.amplifyapp.com'
+//   );
+// };
 
-const signIn = async () => {
-  try {
-    console.log('before federatedSignin')
-    await Auth.federatedSignIn();
-    console.log('after federatedSignin')
-  } catch (error) {
-    console.log("error signing in", error);
-  }
-};
+// const signIn = async () => {
+//   try {
+//     console.log('before federatedSignin')
+//     await Auth.federatedSignIn();
+//     console.log('after federatedSignin')
+//   } catch (error) {
+//     console.log("error signing in", error);
+//   }
+// };
 
 function App() {
-  const [user, setUser] = useState(null);
+  // const [user, setUser] = useState(null);
 
-  Hub.listen('auth', (data) => {
-    console.log('got auth event')
-    const { payload } = data
-    console.log(payload)
-  })
+  // Hub.listen('auth', (data) => {
+  //   console.log('got auth event')
+  //   const { payload } = data
+  //   console.log(payload)
+  // })
 
-  useEffect(() => {
-    console.log('in useEffect')
-    getUser().then((userData) => setUser(userData));
-  }, []);
+  // useEffect(() => {
+  //   console.log('in useEffect')
+  //   getUser().then((userData) => setUser(userData));
+  // }, []);
 
-  function getUser() {
-    console.log('getting current user')
-    return Auth.currentAuthenticatedUser()
-      .then((userData) => userData)
-      .catch((e) => console.log("Not signed in", e));
-  }
+  // function getUser() {
+  //   console.log('getting current user')
+  //   return Auth.currentAuthenticatedUser()
+  //     .then((userData) => userData)
+  //     .catch((e) => console.log("Not signed in", e));
+  // }
 
   return (
-    <div className="App">
-      <h1>
-        Welcome{" "}
-        {user
-          ? JSON.stringify(user)
-          : "Oops! Not signed in"}
-      </h1>
-      <header className="App-header">
-        <button onClick={checkUser}>Check User</button>
-        <button onClick={() => Auth.federatedSignIn()}>Sign In 1</button>
-        <button onClick={openSaml}>Sign In 2</button>
-        <button onClick={signIn}>Sign In 3</button>
-      </header>
-    </div>
-    // <RouterProvider router={router} />;
+    // <div className="App">
+    //   <h1>
+    //     Welcome{" "}
+    //     {user
+    //       ? JSON.stringify(user)
+    //       : "Oops! Not signed in"}
+    //   </h1>
+    //   <header className="App-header">
+    //     <button onClick={checkUser}>Check User</button>
+    //     <button onClick={() => Auth.federatedSignIn()}>Sign In 1</button>
+    //     <button onClick={openSaml}>Sign In 2</button>
+    //     <button onClick={signIn}>Sign In 3</button>
+    //   </header>
+    // </div>
+    <RouterProvider router={router} />;
   )
 }
 
-// export default withAuthenticator(App, { 
-//   hideSignUp: true,
-//   socialProviders: ['apple']
-// });
-export default App
+export default withAuthenticator(App, { 
+  hideSignUp: true
+});
+// export default App
